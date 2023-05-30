4457695217164003f35151b0df006ced3eb49c3d
Vladimir Dolgiy <vladimirdolgijv@gmail.com>
2023-05-30 23:57:02 +0300

    add new eureka client

---
+++ eureka-client-2/src/main/java/org/spring/micro/demo/eurekaclient2/controller/TestController.java
@@ -0,0 +1,15 @@
+package org.spring.micro.demo.eurekaclient2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/new")
+public class TestController {
+
+    @GetMapping("/greeting")
+    public String test(){
+        return "Hello from client 2";
+    }
+}
